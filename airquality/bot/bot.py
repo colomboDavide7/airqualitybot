a4e963d18675e546f0e1b135d453e8ca115b147e
Davide Colombo <davide.colombo02@universitadipavia.it>
2021-10-22 08:45:08 +0200

    Add code in 'run()' method in BotAtmotube for getting sensor ids.

--- airquality/bot/bot.py
+++ airquality/bot/bot.py
@@ -7,6 +7,7 @@
 #################################################
 import builtins
 from abc import ABC, abstractmethod
+from airquality.parser.db_answer_parser import DatabaseAnswerParser
 from airquality.database.sql_query_builder import SQLQueryBuilder
 from airquality.database.db_conn_adapter import ConnectionAdapter
 from airquality.api.api_request_adapter import APIRequestAdapter
@@ -69,6 +70,16 @@ class BotAtmotube(BaseBot):
         # TRY TO OPEN DATABASE ADAPTER CONNECTION
         self.dbconn.open_conn()
         Session.get_current_session().debug_msg(f"{BotAtmotube.__name__}: try to open connection to database: OK")
+
+        # TRY TO GET SENSOR IDs
+        query = self.sqlbuilder.select_sensor_ids_from_identifier("atmotube")
+        answer = self.dbconn.send(query)
+        ids = DatabaseAnswerParser.parse_single_attribute_answer(answer)
+        Session.get_current_session().debug_msg(f"{BotAtmotube.__name__}: try to select sensor ids from identifier 'atmotube': OK")
+
+        # TRY TO GET SENSOR API PARAMETERS
+        
+
 
 
 ################################ FACTORY ################################
