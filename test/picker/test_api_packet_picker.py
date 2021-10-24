84f66fe86dc4175f51d4d441e27051536bf8288a
Davide Colombo <davide.colombo02@universitadipavia.it>
2021-10-24 18:27:14 +0200

    Delete redundant test in 'test_api_packet_picker.py'

--- test/picker/test_api_packet_picker.py
+++ test/picker/test_api_packet_picker.py
@@ -15,22 +15,24 @@ from airquality.picker.api_packet_picker import APIPacketPicker
 class TestAPIPacketPicker(unittest.TestCase):
 
 
-    def test_pick_atmotube_packet_without_geom(self):
-        test_packet = [
-            {"param1":  "value1",
-             "time":    "2021-10-11T09:44:00.000Z"
-             }]
+    def test_pick_only_packets_after_timestamp(self):
+        test_packets = [{"param1": "value1",
+                        "time": "2021-10-11T09:44:00.000Z"
+                         },
+                        {"param1": "value1",
+                         "time": "2021-10-11T09:45:00.000Z"
+                         }]
 
-        test_param_id_code = {"param1": 1}
+        test_param_id_codes = {"param1": 1}
 
         expected_answer = [{f"{PARAM_ID}": 1,
                             f"{PARAM_VALUE}": "'value1'",
-                            f"{TIMESTAMP}": "'2021-10-11 09:44:00'",
+                            f"{TIMESTAMP}": "'2021-10-11 09:45:00'",
                             f"{GEOMETRY}": "null"}]
-
-        actual_answer = APIPacketPicker.pick_atmotube_api_packet(
-                parsed_api_answer = test_packet,
-                param_id_code = test_param_id_code
+        actual_answer = APIPacketPicker.pick_atmotube_api_packets_from_last_timestamp_on(
+                parsed_api_answer = test_packets,
+                param_id_code = test_param_id_codes,
+                last_timestamp = "2021-10-11 09:44:00"
         )
         self.assertEqual(actual_answer, expected_answer)
 
@@ -40,6 +42,7 @@ class TestAPIPacketPicker(unittest.TestCase):
                           "param2": "val2"}
         with self.assertRaises(SystemExit):
             APIPacketPicker.pick_last_atmotube_measure_timestamp_or_empty_string(test_api_param)
+
 
     def test_return_empty_string_when_date_is_None(self):
         test_api_param = {"param1": "val1",
