1410eb610ea2928f6faaa6eeb5b05b91516cc232
Davide Colombo <davide.colombo02@universitadipavia.it>
2021-10-26 17:57:57 +0200

    Refactor string documentation url querystring builder: src + test

--- airquality/api/url_querystring_builder.py
+++ airquality/api/url_querystring_builder.py
@@ -9,7 +9,8 @@ import builtins
 from typing import Dict, Any
 from abc import ABC, abstractmethod
 from airquality.parser.datetime_parser import DatetimeParser
-from airquality.constants.shared_constants import EMPTY_DICT
+from airquality.constants.shared_constants import EMPTY_DICT, \
+    PURPLEAIR_FIELDS_PARAM
 
 
 KEY_VAL_SEPARATOR = "="
@@ -33,7 +34,10 @@ class URLQuerystringBuilderAtmotube(URLQuerystringBuilder):
         Required parameters are:
             - api_key:  the user's private api key
             - mac:      the sensor's mac address
-            - date:     the date from which fetching data
+            - date:     the date from which fetching data (None | 'YYYY-mm-dd HH:MM:SS')
+
+        If 'date' param is None, date is not included into the querystring and this cause the program to download ALL
+        THE HISTORICAL RECORDS OF THE SENSOR from the API.
 
         Optional parameters are:
             - order:    the order in which data are fetched (asc | desc)
@@ -66,7 +70,7 @@ class URLQuerystringBuilderPurpleair(URLQuerystringBuilder):
 
 
     def make_querystring(self, parameters: Dict[str, Any]) -> str:
-        """This method defines the rules for building purple air URL querystring for fetching data from API.
+        """This method defines the rules for building purpleair URL querystring for fetching data from API.
 
         Required parameters are:
             - api_key:  private purple air api key
@@ -79,12 +83,12 @@ class URLQuerystringBuilderPurpleair(URLQuerystringBuilder):
 
         if parameters != EMPTY_DICT:
 
-            if 'api_key' not in keys or 'fields' not in keys:
+            if 'api_key' not in keys or PURPLEAIR_FIELDS_PARAM not in keys:
                 raise SystemExit(f"{URLQuerystringBuilderPurpleair.__name__}: missing 'api_key' or 'fields' keys.")
 
             querystring += 'api_key' + KEY_VAL_SEPARATOR + parameters['api_key'] + CONCAT_SEPARATOR
-            querystring += 'fields' + KEY_VAL_SEPARATOR
-            for field in parameters['fields']:
+            querystring += PURPLEAIR_FIELDS_PARAM + KEY_VAL_SEPARATOR
+            for field in parameters[PURPLEAIR_FIELDS_PARAM]:
                 querystring += field + ','
             querystring = querystring.strip(',') + CONCAT_SEPARATOR
 
@@ -102,7 +106,7 @@ class URLQuerystringBuilderFactory(builtins.object):
     @classmethod
     def create_querystring_builder(cls, bot_personality: str) -> URLQuerystringBuilder:
         """Simple factory method that returns a specific instance of URLQuerystringBuilder based on
-        'bot_personality'."""
+        'bot_personality' argument."""
 
         if bot_personality == "atmotube":
             return URLQuerystringBuilderAtmotube()
--- test/api/test_url_querystring_builder.py
+++ test/api/test_url_querystring_builder.py
@@ -13,48 +13,38 @@ from airquality.api.url_querystring_builder import URLQuerystringBuilderFactory
 class TestBuilder(unittest.TestCase):
 
 
-    def setUp(self) -> None:
-        """Every time a test is run, create the builders."""
-        self.factory = URLQuerystringBuilderFactory()
+    ################################ ATMOTUBE QUERYSTRING BUILDER TESTS ################################
 
-
-################################ ATMOTUBE QUERYSTRING BUILDER TESTS ################################
-
-
-    def test_atmotube_querystring_from_date(self):
-        """Test build atmotube querystring from date."""
+    def test_successfully_build_atmotube_querystring(self):
 
         test_kwargs = {"api_key": "some_api_key", "mac": "some_mac_address",
                        "date": "2021-10-11 17:46:00", "order": "desc"}
         expected_querystring = "api_key=some_api_key&mac=some_mac_address&date=2021-10-11&order=desc"
-        atmotube_builder = self.factory.create_querystring_builder(bot_personality = "atmotube")
+        atmotube_builder = URLQuerystringBuilderFactory().create_querystring_builder(bot_personality = "atmotube")
         actual_querystring = atmotube_builder.make_querystring(parameters = test_kwargs)
         self.assertEqual(actual_querystring, expected_querystring)
 
 
     def test_skip_date_when_date_is_none_atmotube_querystring(self):
-        """Test build atmotube querystring from date with invalid arguments."""
+        """Test valid querystring without 'date' when date is None."""
 
-        test_kwargs = {"api_key": "some_api_key",
-                       "mac": "some_mac_address",
-                       "date": None}
+        test_kwargs = {"api_key": "some_api_key", "mac": "some_mac_address", "date": None}
         expected_querystring = "api_key=some_api_key&mac=some_mac_address"
-        atmotube_builder = self.factory.create_querystring_builder(bot_personality = "atmotube")
+
+        atmotube_builder = URLQuerystringBuilderFactory().create_querystring_builder(bot_personality = "atmotube")
         actual_querystring = atmotube_builder.make_querystring(parameters = test_kwargs)
         self.assertEqual(actual_querystring, expected_querystring)
 
 
-    def test_system_exit_atmotube_from_date(self):
+    def test_system_exit_when_building_atmotube_querystring(self):
         """Test SystemExit when missing required parameters for building atmotube querystring."""
 
-        test_kwargs = {"api_key": "some_api_key",
-                       "date": "2021-10-11 09:44:00"}
-        atmotube_builder = self.factory.create_querystring_builder(bot_personality = "atmotube")
+        test_kwargs = {"api_key": "some_api_key", "date": "2021-10-11 09:44:00"}
+        atmotube_builder = URLQuerystringBuilderFactory().create_querystring_builder(bot_personality = "atmotube")
         with self.assertRaises(SystemExit):
             atmotube_builder.make_querystring(parameters = test_kwargs)
 
-        test_kwargs = {"api_key": "some_api_key",
-                       "mac": "some_mac_address"}
+        test_kwargs = {"api_key": "some_api_key", "mac": "some_mac_address"}
         with self.assertRaises(SystemExit):
             atmotube_builder.make_querystring(parameters = test_kwargs)
 
@@ -62,21 +52,21 @@ class TestBuilder(unittest.TestCase):
 ################################ PURPLE AIR QUERYSTRING BUILDER TESTS ################################
 
 
-    def test_build_purpleair_querystring(self):
+    def test_successfully_build_purpleair_querystring(self):
         """Test the build of a valid purpleair URL querystring."""
 
         test_param = {"api_key": "key", "fields": ["f1", "f2"], "opt": "val"}
         expected_output = "api_key=key&fields=f1,f2&opt=val"
-        purpleair_builder = self.factory.create_querystring_builder(bot_personality = "purpleair")
+        purpleair_builder = URLQuerystringBuilderFactory().create_querystring_builder(bot_personality = "purpleair")
         actual_output = purpleair_builder.make_querystring(parameters = test_param)
         self.assertEqual(actual_output, expected_output)
 
 
-    def test_system_exit_missing_parameters_build_purpleair_querystring(self):
-        """Test SystemExit when missing required purple air parameters."""
+    def test_system_exit_when_building_purpleair_querystring(self):
+        """Test SystemExit when missing required purpleair parameters for building URL querystring."""
 
         test_param = {"fields": "f1", "opt": "val"}
-        purpleair_builder = self.factory.create_querystring_builder(bot_personality = "purpleair")
+        purpleair_builder = URLQuerystringBuilderFactory().create_querystring_builder(bot_personality = "purpleair")
         with self.assertRaises(SystemExit):
             purpleair_builder.make_querystring(parameters = test_param)
 
