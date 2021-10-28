a18bd8cdff51841788fbb24a4136d804d31e9a3f
Davide Colombo <davide.colombo02@universitadipavia.it>
2021-10-28 07:47:58 +0200

    Add tests to api2db station reshaper

--- test/reshaper/test_api2db_station_reshaper.py
+++ test/reshaper/test_api2db_station_reshaper.py
@@ -48,6 +48,29 @@ class TestAPI2DatabaseStationReshaper(unittest.TestCase):
         self.assertEqual(actual_output, expected_output)
 
 
+    def test_system_exit_when_reshaping_with_empty_mapping_thingspeak(self):
+
+        test_api_answer = [{"par1": "val1", "par2": "val2", "time": "ts1"},
+                           {"par1": "val3", "par2": "val4", "time": "ts2"}]
+
+        test_mapping = {}
+
+        reshaper = API2DatabaseStationReshaperFactory().create_reshaper(bot_personality = "thingspeak")
+        with self.assertRaises(SystemExit):
+            reshaper.reshape_packets(packets = test_api_answer, sensor_id = 1, measure_param_map = test_mapping)
+
+
+    def test_empty_list_when_empty_packets_is_passed_thingspeak_reshaper(self):
+
+        test_api_answer = []
+
+        test_mapping = {"par1": 1, "par2": 2}
+
+        reshaper = API2DatabaseStationReshaperFactory().create_reshaper(bot_personality = "thingspeak")
+        actual_output = reshaper.reshape_packets(packets = test_api_answer, sensor_id = 1,
+                                                 measure_param_map = test_mapping)
+        self.assertEqual(actual_output, [])
+
 
 if __name__ == '__main__':
     unittest.main()
