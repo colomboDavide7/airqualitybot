dbef116cfd010381260815b3a6db9b6b7f2d856b
Davide Colombo <davide.colombo02@universitadipavia.it>
2021-11-02 20:32:29 +0100

    Refactor resource picker: src + test

--- airquality/picker/resource_picker.py
+++ airquality/picker/resource_picker.py
@@ -7,9 +7,7 @@
 #################################################
 import builtins
 from typing import Dict, Any, List
-from airquality.constants.shared_constants import PURPLE_AIR_API_PARAM, PURPLE_AIR_GEO_PARAM, EMPTY_LIST, \
-    MOBILE_SENSOR_PERSONALITIES, PURPLEAIR_SENSOR_IDX_PARAM, PURPLEAIR_NAME_PARAM, \
-    ATMOTUBE_DATE_PARAM, ATMOTUBE_PERSONALITY, PURPLEAIR_PERSONALITY, ATMOTUBE_OPTIONAL_API_PARAM
+from airquality.constants.shared_constants import ATMOTUBE_PERSONALITY, ATMOTUBE_OPTIONAL_API_PARAM
 
 LOGGER_SECTION = "logger"
 SERVER_SECTION = "server"
@@ -48,68 +46,6 @@ class ResourcePicker(builtins.object):
         settings["username"] = parsed_resources[PERSONALITY_SECTION][f"{bot_personality}"]["username"]
         settings["password"] = parsed_resources[PERSONALITY_SECTION][f"{bot_personality}"]["password"]
         return settings
-
-    @classmethod
-    def pick_api_address_from_number(cls, parsed_resources: Dict[str, Any], bot_personality: str,
-                                     api_address_number: str) -> str:
-
-        if parsed_resources.get(API_ADDRESS_SECTION, None) is None:
-            raise SystemExit(f"{ResourcePicker.__name__}: "
-                             f"missing '{API_ADDRESS_SECTION}' section in resource file in method "
-                             f"'{ResourcePicker.pick_api_address_from_number.__name__}()'."
-                             f"Check your 'properties/resources.json' file.")
-
-        if bot_personality not in parsed_resources[API_ADDRESS_SECTION].keys():
-            raise SystemExit(f"{ResourcePicker.__name__}: "
-                             f"don't recognize personality '{bot_personality}' in method "
-                             f"'{ResourcePicker.pick_api_address_from_number.__name__}()'."
-                             f"Check your 'properties/resources.json' file.")
-
-        if api_address_number not in parsed_resources[API_ADDRESS_SECTION][f"{bot_personality}"].keys():
-            raise SystemExit(f"{ResourcePicker.__name__}: "
-                             f"don't recognize api address number '{bot_personality}' in method "
-                             f"'{ResourcePicker.pick_api_address_from_number.__name__}()'."
-                             f"Check your 'properties/resources.json' file.")
-
-        return parsed_resources[API_ADDRESS_SECTION][f"{bot_personality}"][f"{api_address_number}"]
-
-    @classmethod
-    def pick_api_param_filter_list_from_personality(cls, bot_personality: str) -> List[str]:
-
-        if bot_personality == "purpleair":
-            return PURPLE_AIR_API_PARAM
-        else:
-            return EMPTY_LIST
-
-    @classmethod
-    def pick_geo_param_filter_list_from_personality(cls, personality: str) -> List[str]:
-        if personality == "purpleair":
-            return PURPLE_AIR_GEO_PARAM
-        else:
-            return EMPTY_LIST
-
-    @classmethod
-    def pick_last_timestamp_from_api_param_by_personality(cls, api_param: Dict[str, Any], personality: str) -> str:
-
-        if personality not in MOBILE_SENSOR_PERSONALITIES:
-            raise SystemExit(f"{ResourcePicker.pick_last_timestamp_from_api_param_by_personality.__name__}: "
-                             f"cannot call this method with personality = '{personality}'.")
-
-        sqltimestamp = ""
-        if personality == ATMOTUBE_PERSONALITY:
-            if api_param.get(ATMOTUBE_DATE_PARAM, None) is not None:
-                sqltimestamp = api_param[ATMOTUBE_DATE_PARAM]
-
-        return sqltimestamp
-
-    @classmethod
-    def pick_sensor_name_from_identifier(cls, packet: Dict[str, Any], personality: str) -> str:
-
-        if personality == PURPLEAIR_PERSONALITY:
-            return f"{packet[PURPLEAIR_NAME_PARAM]} ({packet[PURPLEAIR_SENSOR_IDX_PARAM]})"
-        else:
-            raise SystemExit(f"{ResourcePicker.pick_sensor_name_from_identifier.__name__}: cannot pick name for "
-                             f"personality = '{personality}'.")
 
     @classmethod
     def pick_optional_api_parameters_from_api_data(cls, personality: str) -> List[str]:
--- test/picker/test_resource_picker.py
+++ test/picker/test_resource_picker.py
@@ -20,7 +20,6 @@ class TestResourcePicker(unittest.TestCase):
                           "pers1": {"username": "usr1", "password": "pwd1"}
                       }}
 
-
         expected_output = {"key1": "val1", "key2": "val2", "username": "usr1", "password": "pwd1"}
         actual_output = ResourcePicker.pick_db_conn_properties(parsed_res, "pers1")
         self.assertEqual(actual_output, expected_output)
@@ -38,53 +37,6 @@ class TestResourcePicker(unittest.TestCase):
         parsed_res = {"servers": {}}
         with self.assertRaises(SystemExit):
             ResourcePicker.pick_db_conn_properties(parsed_res, "pers1")
-
-    def test_pick_api_address_from_number(self):
-        parsed_res = {"api_address": {
-                          "pers1": {"1": "api_address_1", "2": "api_address_2"}
-                      }}
-        expected_output = "api_address_1"
-        actual_output = ResourcePicker.pick_api_address_from_number(
-                parsed_resources = parsed_res,
-                bot_personality = "pers1",
-                api_address_number = "1"
-        )
-
-        self.assertEqual(actual_output, expected_output)
-
-    def test_system_exit_missing_api_address_section(self):
-        parsed_res = {}
-        with self.assertRaises(SystemExit):
-            ResourcePicker.pick_api_address_from_number(
-                    parsed_resources = parsed_res,
-                    bot_personality = "pers1",
-                    api_address_number = "1"
-            )
-
-    def test_system_exit_when_wrong_personality_is_passed(self):
-        parsed_res = {"api_address": {
-            "pers1": {"1": "api_address_1", "2": "api_address_2"}
-        }}
-
-        with self.assertRaises(SystemExit):
-            ResourcePicker.pick_api_address_from_number(
-                    parsed_resources = parsed_res,
-                    bot_personality = "bad_personality",
-                    api_address_number = "1"
-            )
-
-    def test_system_exit_when_wrong_api_number(self):
-        parsed_res = {"api_address": {
-            "pers1": {"1": "api_address_1", "2": "api_address_2"}
-        }}
-
-        with self.assertRaises(SystemExit):
-            ResourcePicker.pick_api_address_from_number(
-                    parsed_resources = parsed_res,
-                    bot_personality = "pers1",
-                    api_address_number = "4"
-            )
-
 
 
 if __name__ == '__main__':
