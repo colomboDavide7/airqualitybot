206bb1da314b09c6289e5a72154df0826bec8c9e
Davide Colombo <davide.colombo02@universitadipavia.it>
2021-11-02 21:01:11 +0100

    Bug fixed in dict2mobile reshaper

--- airquality/reshaper/dict2mobilepacket_reshaper.py
+++ airquality/reshaper/dict2mobilepacket_reshaper.py
@@ -40,8 +40,9 @@ class Dict2MobilepacketReshaperAtmotube(Dict2MobilepacketReshaper):
             timestamp = DatetimeParser.atmotube_to_sqltimestamp(packet[ATMOTUBE_TIME_PARAM])
             geom = "null"
             if ATMOTUBE_COORDS_PARAM in packet.keys():
-                geom = PostGISPointFactory(lat=packet[ATMOTUBE_COORDS_PARAM]["lat"],
-                                           lng=packet[ATMOTUBE_COORDS_PARAM]["lon"]).create_geometry()
+                point = PostGISPointFactory(lat=packet[ATMOTUBE_COORDS_PARAM]["lat"],
+                                            lng=packet[ATMOTUBE_COORDS_PARAM]["lon"]).create_geometry()
+                geom = point.get_database_string()
 
             for name, val in packet.items():
                 if name in reshape_mapping.keys():
@@ -52,40 +53,6 @@ class Dict2MobilepacketReshaperAtmotube(Dict2MobilepacketReshaper):
         return reshaped_packets
 
 
-# class API2DatabaseReshaperPurpleair(Dict2MobilepacketReshaper):
-#
-#     def reshape_packets(self, packets: List[Dict[str, Any]], reshape_mapping: Dict[str, Any]) -> List[GeoPacket]:
-#
-#         if packets == EMPTY_LIST:
-#             return []
-#
-#         if reshape_mapping == EMPTY_DICT:
-#             raise SystemExit(f"{API2DatabaseReshaperPurpleair.__name__}: cannot reshape packets when reshape mapping "
-#                              f"is empty.")
-#
-#         reshaped_packets = []
-#         for packet in packets:
-#             # GET THE PACKET KEYS TO AVOID CALLING THE METHOD MULTIPLE TIMES
-#             keys = packet.keys()
-#
-#             # CHECK IF THERE ARE LATITUDE AND LONGITUDE INSIDE THE PACKET
-#             if PURPLEAIR_LAT_PARAM not in keys or PURPLEAIR_LON_PARAM not in keys:
-#                 raise SystemExit(f"{API2DatabaseReshaperPurpleair.__name__}: missing purpleair geolocation parameters.")
-#
-#             geom = PostGISGeomBuilder.postgisgeom2geostring(PostGISPoint(lat=packet[PURPLEAIR_LAT_PARAM],
-#                                                                          lng=packet[PURPLEAIR_LON_PARAM]))
-#
-#             # CHECK IF SENSOR NAME PARAMETERS ARE MISSING OR NOT
-#             if PURPLEAIR_NAME_PARAM not in keys or PURPLEAIR_SENSOR_IDX_PARAM not in keys:
-#                 raise SystemExit(f"{API2DatabaseReshaperPurpleair.__name__}: missing purpleair name parameters.")
-#             sensor_name = f"{packet[PURPLEAIR_NAME_PARAM]} ({packet[PURPLEAIR_SENSOR_IDX_PARAM]})"
-#
-#             if sensor_name in reshape_mapping.keys():
-#                 reshaped_packets.append(GeoPacket(sensor_id=reshape_mapping[sensor_name], geom=geom))
-#
-#         return reshaped_packets
-
-
 ################################ FACTORY ################################
 class Dict2MobilepacketReshaperFactory(builtins.object):
 
@@ -94,8 +61,6 @@ class Dict2MobilepacketReshaperFactory(builtins.object):
 
         if bot_personality == "atmotube":
             return Dict2MobilepacketReshaperAtmotube()
-        # elif bot_personality == "purpleair":
-        #     return API2DatabaseReshaperPurpleair()
         else:
             raise SystemExit(
                 f"{Dict2MobilepacketReshaperFactory.__name__}: cannot instantiate {Dict2MobilepacketReshaper.__name__} "
--- test/reshaper/test_dict2mobilepacket_reshaper.py
+++ test/reshaper/test_dict2mobilepacket_reshaper.py
@@ -49,44 +49,6 @@ class TestDict2MobilepacketReshaper(unittest.TestCase):
         with self.assertRaises(SystemExit):
             reshaper.reshape_packets(packets = test_packets, reshape_mapping = test_code2id_map)
 
-    ################################ PURPLE AIR RESHAPER ################################
-    #
-    # def test_successfully_reshape_purpleair_packets(self):
-    #     test_packets = [{"name": "n1", "sensor_index": "idx1", "latitude": "45.123", "longitude": "8.673"},
-    #                     {"name": "n2", "sensor_index": "idx2", "latitude": "45.456", "longitude": "8.123"},
-    #                     {"name": "n3", "sensor_index": "idx3", "latitude": "45.789", "longitude": "8.456"}]
-    #
-    #     test_reshape_map = {"n1 (idx1)": "9", "n2 (idx2)": "5", "n3 (idx3)": "14"}
-    #
-    #     expected_output = [{"9": "ST_GeomFromText('POINT(8.673 45.123)')"},
-    #                        {"5": "ST_GeomFromText('POINT(8.123 45.456)')"},
-    #                        {"14": "ST_GeomFromText('POINT(8.456 45.789)')"}]
-    #     reshaper = Dict2MobilepacketReshaperFactory().create_api2database_reshaper(bot_personality ="purpleair")
-    #     actual_output = reshaper.reshape_packets(packets = test_packets, reshape_mapping = test_reshape_map)
-    #     self.assertEqual(actual_output, expected_output)
-    #
-    # def test_system_exit_when_missing_name_param_purpleair_reshaper(self):
-    #     test_packets = [{"latitude": "45.123", "longitude": "8.673"},
-    #                     {"latitude": "45.456", "longitude": "8.123"},
-    #                     {"latitude": "45.789", "longitude": "8.456"}]
-    #
-    #     test_reshape_map = {"n1 (idx1)": "9", "n2 (idx2)": "5", "n3 (idx3)": "14"}
-    #
-    #     reshaper = Dict2MobilepacketReshaperFactory().create_api2database_reshaper(bot_personality ="purpleair")
-    #     with self.assertRaises(SystemExit):
-    #         reshaper.reshape_packets(packets = test_packets, reshape_mapping = test_reshape_map)
-    #
-    # def test_system_exit_when_missing_geolocation_param_purpleair_reshaper(self):
-    #     test_packets = [{"name": "n1", "sensor_index": "idx1"},
-    #                     {"name": "n2", "sensor_index": "idx2"},
-    #                     {"name": "n3", "sensor_index": "idx3"}]
-    #
-    #     test_reshape_map = {"n1 (idx1)": "9", "n2 (idx2)": "5", "n3 (idx3)": "14"}
-    #
-    #     reshaper = Dict2MobilepacketReshaperFactory().create_api2database_reshaper(bot_personality ="purpleair")
-    #     with self.assertRaises(SystemExit):
-    #         reshaper.reshape_packets(packets = test_packets, reshape_mapping = test_reshape_map)
-
 
 if __name__ == '__main__':
     unittest.main()
