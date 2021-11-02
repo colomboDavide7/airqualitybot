78b5cd85c4f767f98584463aa5bd9b24b475c6fd
Davide Colombo <davide.colombo02@universitadipavia.it>
2021-11-02 20:48:15 +0100

    Refactor test identifier filter

--- test/filter/test_identifier_packet_filter.py
+++ test/filter/test_identifier_packet_filter.py
@@ -8,43 +8,41 @@
 
 import unittest
 from airquality.filter.identifier_packet_filter import IdentifierPacketFilterFactory
+from airquality.packet.apiparam_single_packet import APIParamSinglePacketPurpleair
 from airquality.constants.shared_constants import EMPTY_LIST
 
 
 class TestIdentifierPacketFilter(unittest.TestCase):
 
-
     def test_successfully_filter_purpleair_packets(self):
         """This method tests the correct behaviour of the purpleair filter class."""
 
-        test_packets = [{"name": "n1", "sensor_index": "idx1"},
-                        {"name": "n2", "sensor_index": "idx2"},
-                        {"name": "n3", "sensor_index": "idx3"}]
+        test_packets = [APIParamSinglePacketPurpleair({"name": "n1", "sensor_index": "idx1"}),
+                        APIParamSinglePacketPurpleair({"name": "n2", "sensor_index": "idx2"}),
+                        APIParamSinglePacketPurpleair({"name": "n3", "sensor_index": "idx3"})]
 
         test_filter_list = ["n1 (idx1)"]
-        expected_output = [{"name": "n2", "sensor_index": "idx2"},
-                           {"name": "n3", "sensor_index": "idx3"}]
-        filter_ = IdentifierPacketFilterFactory.create_identifier_filter(bot_personality = "purpleair")
-        actual_output = filter_.filter_packets(packets = test_packets, identifiers = test_filter_list)
+        expected_output = [APIParamSinglePacketPurpleair({"name": "n2", "sensor_index": "idx2"}),
+                           APIParamSinglePacketPurpleair({"name": "n3", "sensor_index": "idx3"})]
+        filter_ = IdentifierPacketFilterFactory.create_identifier_filter(bot_personality="purpleair")
+        actual_output = filter_.filter_packets(packets=test_packets, identifiers=test_filter_list)
         self.assertEqual(actual_output, expected_output)
-
 
     def test_purpleair_filter_with_empty_filter_list(self):
         """This method tests the behaviour of the purpleair filter method with 'filter_list' argument equal to
         'EMPTY_LIST'."""
 
-        test_packets = [{"name": "n1", "sensor_index": "idx1"},
-                        {"name": "n2", "sensor_index": "idx2"},
-                        {"name": "n3", "sensor_index": "idx3"}]
+        test_packets = [APIParamSinglePacketPurpleair({"name": "n1", "sensor_index": "idx1"}),
+                        APIParamSinglePacketPurpleair({"name": "n2", "sensor_index": "idx2"}),
+                        APIParamSinglePacketPurpleair({"name": "n3", "sensor_index": "idx3"})]
 
-        test_filter_list = EMPTY_LIST
-        expected_output = [{"name": "n1", "sensor_index": "idx1"},
-                           {"name": "n2", "sensor_index": "idx2"},
-                           {"name": "n3", "sensor_index": "idx3"}]
-        filter_ = IdentifierPacketFilterFactory.create_identifier_filter(bot_personality = "purpleair")
-        actual_output = filter_.filter_packets(packets = test_packets, identifiers = test_filter_list)
+        test_filter_list = []
+        expected_output = [APIParamSinglePacketPurpleair({"name": "n1", "sensor_index": "idx1"}),
+                           APIParamSinglePacketPurpleair({"name": "n2", "sensor_index": "idx2"}),
+                           APIParamSinglePacketPurpleair({"name": "n3", "sensor_index": "idx3"})]
+        filter_ = IdentifierPacketFilterFactory.create_identifier_filter(bot_personality="purpleair")
+        actual_output = filter_.filter_packets(packets=test_packets, identifiers=test_filter_list)
         self.assertEqual(actual_output, expected_output)
-
 
     def test_purpleair_filter_with_empty_packets(self):
         """This method tests the return value of 'EMPTY_LIST' an empty list of packets is passed as argument."""
@@ -52,10 +50,9 @@ class TestIdentifierPacketFilter(unittest.TestCase):
         test_packets = EMPTY_LIST
         test_filter_list = ["something1", "something2"]
         expected_output = EMPTY_LIST
-        filter_ = IdentifierPacketFilterFactory.create_identifier_filter(bot_personality = "purpleair")
-        actual_output = filter_.filter_packets(packets = test_packets, identifiers = test_filter_list)
+        filter_ = IdentifierPacketFilterFactory.create_identifier_filter(bot_personality="purpleair")
+        actual_output = filter_.filter_packets(packets=test_packets, identifiers=test_filter_list)
         self.assertEqual(actual_output, expected_output)
-
 
 
 if __name__ == '__main__':
