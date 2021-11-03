e7f320c1e50183db5577aa6bdef9798aae174f0a
Davide Colombo <davide.colombo02@universitadipavia.it>
2021-11-03 19:53:30 +0100

    Refactor datetime packet filter test

--- test/filter/test_datetime_packet_filter.py
+++ test/filter/test_datetime_packet_filter.py
@@ -8,7 +8,7 @@
 
 import unittest
 from airquality.filter.datetime_packet_filter import DatetimePacketFilterFactory
-from airquality.constants.shared_constants import ATMOTUBE_TIME_PARAM
+from airquality.plain.plain_api_packet import PlainAPIPacketAtmotube
 
 
 class TestDatetimeFilter(unittest.TestCase):
@@ -16,39 +16,38 @@ class TestDatetimeFilter(unittest.TestCase):
     def test_successfully_filter_atmotube_packets(self):
         """Test the successful scenario of filtering atmotube packets."""
 
-        test_packets = {"data": {"items": [{ATMOTUBE_TIME_PARAM: "2021-10-11T09:44:00.000Z"},
-                                           {ATMOTUBE_TIME_PARAM: "2021-10-11T09:45:00.000Z"},
-                                           {ATMOTUBE_TIME_PARAM: "2021-10-11T09:46:00.000Z"}]}}
+        test_packets = [PlainAPIPacketAtmotube({'time': "2021-10-11T09:44:00.000Z"}),
+                        PlainAPIPacketAtmotube({'time': "2021-10-11T09:45:00.000Z"}),
+                        PlainAPIPacketAtmotube({'time': "2021-10-11T09:46:00.000Z"})]
 
         test_sqltimestamp = "2021-10-11 09:45:00"
-        expected_output = [{ATMOTUBE_TIME_PARAM: "2021-10-11T09:46:00.000Z"}]
-        filter_ = DatetimePacketFilterFactory().create_datetime_filter(bot_personality="atmotube")
-        actual_output = filter_.filter_packets(packets=test_packets, sqltimestamp=test_sqltimestamp)
+        expected_output = [PlainAPIPacketAtmotube({'time': "2021-10-11T09:46:00.000Z"})]
+        datetime_filter = DatetimePacketFilterFactory().create_datetime_filter(bot_personality="atmotube")
+        actual_output = datetime_filter.filter_packets(packets=test_packets, sqltimestamp=test_sqltimestamp)
         self.assertEqual(actual_output, expected_output)
 
     def test_same_packets_when_sqltimestamp_is_empty_atmotube_filter(self):
         """Test same items when no 'sqltimestamp' is provided."""
 
-        test_packets = {"data": {"items": [{ATMOTUBE_TIME_PARAM: "2021-10-11T09:44:00.000Z"},
-                                           {ATMOTUBE_TIME_PARAM: "2021-10-11T09:45:00.000Z"},
-                                           {ATMOTUBE_TIME_PARAM: "2021-10-11T09:46:00.000Z"}]}}
-        expected_output = [{ATMOTUBE_TIME_PARAM: "2021-10-11T09:44:00.000Z"},
-                           {ATMOTUBE_TIME_PARAM: "2021-10-11T09:45:00.000Z"},
-                           {ATMOTUBE_TIME_PARAM: "2021-10-11T09:46:00.000Z"}]
+        test_packets = [PlainAPIPacketAtmotube({'time': "2021-10-11T09:44:00.000Z"}),
+                        PlainAPIPacketAtmotube({'time': "2021-10-11T09:45:00.000Z"}),
+                        PlainAPIPacketAtmotube({'time': "2021-10-11T09:46:00.000Z"})]
+        expected_output = [PlainAPIPacketAtmotube({'time': "2021-10-11T09:44:00.000Z"}),
+                           PlainAPIPacketAtmotube({'time': "2021-10-11T09:45:00.000Z"}),
+                           PlainAPIPacketAtmotube({'time': "2021-10-11T09:46:00.000Z"})]
         test_sqltimestamp = ""
-        filter_ = DatetimePacketFilterFactory().create_datetime_filter(bot_personality="atmotube")
-        actual_output = filter_.filter_packets(packets=test_packets, sqltimestamp=test_sqltimestamp)
+        datetime_filter = DatetimePacketFilterFactory().create_datetime_filter(bot_personality="atmotube")
+        actual_output = datetime_filter.filter_packets(packets=test_packets, sqltimestamp=test_sqltimestamp)
         self.assertEqual(actual_output, expected_output)
 
-    def test_empty_list_when_items_is_empty_atmotube_filter(self):
+    def test_empty_list_when_empty_list_is_passed(self):
         """Test empty list return value."""
 
-        test_packets = {"data": {"items": []}}
-
+        test_packets = []
         test_sqltimestamp = "2021-10-11 09:45:00"
         expected_output = []
-        filter_ = DatetimePacketFilterFactory().create_datetime_filter(bot_personality="atmotube")
-        actual_output = filter_.filter_packets(packets=test_packets, sqltimestamp=test_sqltimestamp)
+        datetime_filter = DatetimePacketFilterFactory().create_datetime_filter(bot_personality="atmotube")
+        actual_output = datetime_filter.filter_packets(packets=test_packets, sqltimestamp=test_sqltimestamp)
         self.assertEqual(actual_output, expected_output)
 
 
