6c4179f76d53ef9b8fb38546948314be293d7643
Davide Colombo <davide.colombo02@universitadipavia.it>
2021-11-09 22:58:50 +0100

    Small refactoring in 'initialize_bot'

--- airquality/bot/initialize_bot.py
+++ airquality/bot/initialize_bot.py
@@ -59,71 +59,61 @@ class InitializeBot:
         parsed_packets = self.file_parser.parse(raw_packets)                        # parse API answer
         reshaped_packets = self.packet_reshaper.reshape_packet(parsed_packets)      # reshape API packets
 
-        if reshaped_packets:
-            uniformed_packets = []                                                  # uniformed packets list
-            for packet in reshaped_packets:                                         # for each packet...
-                uniformed_packets.append(self.a2d_reshaper.api2db(packet))          # ... uniform the packet
+        if not reshaped_packets:
+            print(f"{INFO_HEADER} empty API answer")
+            self.dbconn.close_conn()
+            return
 
-            if sc.DEBUG_MODE:
-                print(20 * "=" + " FILTER SENSORS " + 20 * '=')
+        uniformed_packets = []                                                      # uniformed packets list
+        for packet in reshaped_packets:                                             # for each packet...
+            uniformed_packets.append(self.a2d_reshaper.api2db(packet))              # ... uniform the packet
 
-            filtered_packets = []                                                   # filtered packets list
-            for uniformed_packet in uniformed_packets:                              # for each packet...
-                if uniformed_packet['name'] not in sensor_names:                    # ...if is not presents into DB...
-                    filtered_packets.append(uniformed_packet)                       # ...add to the list
-                else:
-                    print(f"{WARNING_HEADER} '{uniformed_packet['name']}' => already present")
+        if sc.DEBUG_MODE:
+            print(20 * "=" + " FILTER SENSORS " + 20 * '=')
 
-            if not filtered_packets:
-                print(f"{INFO_HEADER} all sensors are already present into the database")
-                self.dbconn.close_conn()
-                return
+        filtered_packets = []                                                       # filtered packets list
+        for uniformed_packet in uniformed_packets:                                  # for each packet...
+            if uniformed_packet['name'] not in sensor_names:                        # ...if is not presents into DB...
+                filtered_packets.append(uniformed_packet)                           # ...add to the list
+            else:
+                print(f"{WARNING_HEADER} '{uniformed_packet['name']}' => already present")
 
-            if sc.DEBUG_MODE:
-                print(20 * "=" + " NEW SENSORS " + 20 * '=')
-                for packet in filtered_packets:
-                    print(f"{DEBUG_HEADER} name='{packet['name']}'")
+        if not filtered_packets:
+            print(f"{INFO_HEADER} all sensors are already present into the database")
+            self.dbconn.close_conn()
+            return
 
-            ############################## BUILD SQL FROM FILTERED UNIFORMED PACKETS #############################
-            temp_sensor_id = first_sensor_id
-            sensor_at_location_values = []
-            api_param_values = []
-            sensor_values = []
+        if sc.DEBUG_MODE:
+            print(20 * "=" + " NEW SENSORS " + 20 * '=')
             for packet in filtered_packets:
-                # **************************
-                sensor_values.append(
-                    self.sensor_builder_class(
-                        sensor_id=temp_sensor_id,
-                        packet=packet
-                    )
-                )
-                # **************************
-                geometry = self.geom_builder_class(srid=26918, packet=packet)
-                sensor_at_location_values.append(
-                    self.sens_at_loc_builder_class(
-                        sensor_id=temp_sensor_id,
-                        valid_from=self.current_ts.ts,
-                        geom=geometry.geom_from_text()
-                    )
-                )
-                # **************************
-                api_param_values.append(
-                    self.api_param_builder_class(
-                        sensor_id=temp_sensor_id,
-                        packet=packet
-                    )
-                )
+                print(f"{DEBUG_HEADER} name='{packet['name']}'")
 
-                temp_sensor_id += 1
+        ############################## BUILD SQL FROM FILTERED UNIFORMED PACKETS #############################
+        tmp_id = first_sensor_id
+        sensor_at_location_values = []
+        api_param_values = []
+        sensor_values = []
+        for packet in filtered_packets:
+            # **************************
+            sensor_value = self.sensor_builder_class(sensor_id=tmp_id, packet=packet)
+            sensor_values.append(sensor_value)
+            # **************************
+            geometry = self.geom_builder_class(srid=26918, packet=packet)
+            valid_from = self.current_ts.ts
+            geom = geometry.geom_from_text()
+            geom_value = self.sens_at_loc_builder_class(sensor_id=tmp_id, valid_from=valid_from, geom=geom)
+            sensor_at_location_values.append(geom_value)
+            # **************************
+            api_param_value = self.api_param_builder_class(sensor_id=tmp_id, packet=packet)
+            api_param_values.append(api_param_value)
+            # **************************
+            tmp_id += 1
 
-            ################################ BUILD + EXECUTE QUERIES ################################
-            query = self.query_picker.insert_into_sensor(sensor_values)
-            query += self.query_picker.insert_into_api_param(api_param_values)
-            query += self.query_picker.insert_into_sensor_at_location(sensor_at_location_values)
-            self.dbconn.send(query)
-
-        else:
-            print(f"{INFO_HEADER} empty packets.")
+        ################################ BUILD + EXECUTE QUERIES ################################
+        query = self.query_picker.insert_into_sensor(sensor_values)
+        query += self.query_picker.insert_into_api_param(api_param_values)
+        query += self.query_picker.insert_into_sensor_at_location(sensor_at_location_values)
+        self.dbconn.send(query)
 
         ################################ SAFELY CLOSE DATABASE CONNECTION ################################
         self.dbconn.close_conn()
