b23cce9580645940e00a0403804b60156a1f56aa
Davide Colombo <davide.colombo02@universitadipavia.it>
2021-11-15 08:35:26 +0100

    Remove check on arguments in 'get_bot_class()

--- airquality/bot/util/fact.py
+++ airquality/bot/util/fact.py
@@ -10,28 +10,12 @@ import airquality.bot.init as init
 import airquality.bot.fetch as fetch
 import airquality.bot.update as update
 
-VALID_NAMES = ("init", "update", "fetch")
-VALID_TYPES = ('purpleair', 'thingspeak', 'atmotube')
 
+def get_bot_class(bot_name: str):
 
-def get_bot_class(bot_name: str, sensor_type: str):
-
-    err_msg = f"'{get_bot_class.__name__}()': '{sensor_type}' type is bad for '{bot_name}' bot => "
     if bot_name == 'init':
-        if sensor_type in ('purpleair', ):
-            return init.InitializeBot
-        else:
-            err_msg += f"'{bot_name}' bot valid type is: ['purpleair']"
+        return init.InitializeBot
     elif bot_name == 'update':
-        if sensor_type in ('purpleair', ):
-            return update.UpdateBot
-        else:
-            err_msg += f"'{bot_name}' bot valid type is: ['purpleair']"
+        return update.UpdateBot
     elif bot_name == 'fetch':
-        if sensor_type in ('atmotube', 'thingspeak', ):
-            return fetch.FetchBot
-        else:
-            err_msg += f"'{bot_name}' bot valid types are: ['atmotube', 'thingspeak']"
-
-    # If the interpreter arrive here, it means that the combination of 'bot_name' and 'sensor_type' is wrong
-    raise SystemExit(err_msg)
+        return fetch.FetchBot
--- airquality/runner.py
+++ airquality/runner.py
@@ -19,7 +19,7 @@ import airquality.file.util.parser as txt
 import airquality.database.conn as db
 import airquality.database.util.datatype.timestamp as ts
 import airquality.database.util.postgis.geom as geom
-import airquality.database.util.sql.query as pk
+import airquality.database.util.sql.query as qry
 import airquality.api.util.extractor as ext
 import airquality.api.util.url as url
 import airquality.adapter.api2db.sensor as sens
@@ -37,8 +37,9 @@ API_FILE = "properties/api.json"
 QUERY_FILE = "properties/query.json"
 
 
+################################ MAKE DEBUGGER FUNCTION ################################
 def make_console_debugger(use_color=True):
-    """Function that creates a Logger with a StreamHandler and a ColoredFormatter"""
+    """Function that creates a Logger with a StreamHandler and a ColoredFormatter."""
 
     handler_cls = log.get_handler_cls(use_file=False)
     handler = handler_cls()
@@ -47,8 +48,9 @@ def make_console_debugger(use_color=True):
     return log.get_logger(handler=handler, formatter=fmt)
 
 
+################################ MAKE LOGGER FUNCTION ################################
 def make_file_logger(file_path: str, mode='a+'):
-    """Function that creates a Logger instance with a FileHandler and a CustomFormatter"""
+    """Function that creates a Logger instance with a FileHandler and a CustomFormatter."""
 
     handler_cls = log.get_handler_cls(use_file=True)
     handler = handler_cls(file_path, mode)
@@ -57,47 +59,76 @@ def make_file_logger(file_path: str, mode='a+'):
     return log.get_logger(handler=handler, formatter=fmt)
 
 
-################################ ENTRY POINT OF THE PROGRAM ################################
+################################ MAKE DATABASE ADAPETR FUNCTION ################################
+def make_database_adapter():
+    """Function that checks if the 'DBCONN' key is present in the '.env' file, handles errors in case it will miss
+    and return a database adapter object."""
+
+    if not os.environ.get('DBCONN'):
+        raise SystemExit(f"'{make_database_adapter.__name__}()': bad '.env' file structure => missing param='DBCONN'")
+    return db.Psycopg2DatabaseAdapter(os.environ['DBCONN'])
+
+
+################################ COMMANDLINE ARGUMENTS EXIT FUNCTION ################################
+def exit_on_bad_commandline_arguments(bot_name: str, sensor_type: str):
+    """Function that raises SystemExit if the combination of the two command line arguments is invalid."""
+
+    err_msg = f"'{exit_on_bad_commandline_arguments.__name__}()': bad arguments => "
+    raise_error = False
+
+    if bot_name == 'init':
+        if sensor_type not in ('purpleair',):
+            err_msg += f"'{bot_name}' bot valid type is: ['purpleair']"
+            raise_error = True
+
+    elif bot_name == 'update':
+        if sensor_type not in ('purpleair',):
+            err_msg += f"'{bot_name}' bot valid type is: ['purpleair']"
+            raise_error = True
+
+    elif bot_name == 'fetch':
+        if sensor_type not in ('atmotube', 'thingspeak',):
+            err_msg += f"'{bot_name}' bot valid types are: ['atmotube', 'thingspeak']"
+            raise_error = True
+
+    if raise_error:
+        raise SystemExit(err_msg)
+
+
+################################ LOADING '.env' FILE ################################
+dotenv.load_dotenv(dotenv_path="./properties/.env")
+
+################################ ERROR LOGGER AND DEBUGGER VARIABLES ################################
+error_logger = make_file_logger(file_path='log/errors.log')
+debugger = make_console_debugger(use_color=True)
+message_to_log = []
+
+
+################################ MAIN FUNCTION ################################
 def main():
 
-    # Create an 'error_logger' that keep trace only of errors
-    error_logger = make_file_logger(file_path='log/errors.log')
-
-    # Argument checking
-    args = sys.argv[1:]
-    if not args:
-        error_logger.error(f"'{main.__name__}()': bad usage => missing required arguments")
-        print(USAGE)
-        sys.exit(1)
-
-    bot_name = args[0]              # 'bot name' identifies the broader operation of the bot
-    sensor_type = args[1]           # 'sensor_type' identifies how the operations are specifically handled
-
-    logger = make_file_logger(file_path=f'log/{bot_name}.log')              # Create specific file 'logger'
-    debugger = make_console_debugger(use_color=True)                        # Create console 'debugger'
-
-    messages = []                   # define a list of messages to log both on file and console
-
     try:
-        # Get the 'bot_class' to use in the program
-        bot_class = fact.get_bot_class(bot_name=bot_name, sensor_type=sensor_type)
-        messages.append(f"bot_class={bot_class.__name__}")
-        messages.append(f"sensor_type={sensor_type}")
 
-        # Load '.env' file
-        dotenv.load_dotenv(dotenv_path="./properties/.env")
+        # DISCARD THE COMMANDLINE ARGUMENT AT INDEX 0 BECAUSE IT IS THE NAME OF THE PROGRAM
+        args = sys.argv[1:]
+        if not args or len(args) != 2:
+            print(USAGE)
+            raise SystemExit(f"'{main.__name__}()': bad usage => missing required arguments")
 
-        # Open database connection
-        if not os.environ.get('DBCONN'):
-            raise SystemExit(f"'{main.__name__}()': bad '.env' file structure => missing param='DBCONN'")
-        dbconn = db.Psycopg2DatabaseAdapter(os.environ['DBCONN'])
+        ################################ CHECK COMMAND LINE ARGUMENTS ################################
+        bot_name = args[0]
+        sensor_type = args[1]
+        exit_on_bad_commandline_arguments(bot_name=bot_name, sensor_type=sensor_type)
 
-        # Query file object
-        query_file = struct.JSONFile(QUERY_FILE)
-        query_builder = pk.QueryBuilder(query_file)
+        ################################ BOT SPECIFIC LOGGER ################################
+        logger = make_file_logger(file_path=f'log/{bot_name}.log')
 
-        # API file object
-        api_file = struct.JSONFile(API_FILE, path_to_object=[sensor_type])
+        db_adapter = make_database_adapter()                            # make database adapter for sending query
+
+        query_file = struct.JSONFile(QUERY_FILE)          # read the 'query.json' file and incapsulate it in a JSONFile object
+        query_builder = qry.QueryBuilder(query_file)      # create a QueryBuilder by injecting the JSONFile object
+
+        api_file = struct.JSONFile(API_FILE, path_to_object=[sensor_type])      # read 'api.json' file and make JSONFile object
         address = api_file.api_address
         url_param = api_file.url_param
 
@@ -116,18 +147,20 @@ def main():
             text_parser_class = txt.get_parser_class(url_param['format'])
 
         # Update settings string
-        messages.append(f"text_parser_class={text_parser_class.__name__}")
+        message_to_log.append(f"text_parser_class={text_parser_class.__name__}")
 
         # URLBuilder class
         url_class = url.get_url_class(sensor_type)
         url_builder = url_class(address=address, url_param=url_param)
-        messages.append(f"url_class={url_class.__name__}")
+        message_to_log.append(f"url_class={url_class.__name__}")
 
         # APIExtractor class
         extractor_class = ext.get_api_extractor_class(sensor_type)
-        messages.append(f"extractor_class={extractor_class.__name__}")
+        message_to_log.append(f"extractor_class={extractor_class.__name__}")
 
-        # Create the bot instance
+
+        bot_class = fact.get_bot_class(bot_name=bot_name)
+        message_to_log.append(f"bot_class={bot_class.__name__}")
         bot = bot_class()
 
         # Add external dependencies
@@ -137,7 +170,7 @@ def main():
 
         # Define timest_class
         timest_cls = ts.get_timest_class(sensor_type)
-        messages.append(f"timest_cls={timest_cls.__name__}")
+        message_to_log.append(f"timest_cls={timest_cls.__name__}")
 
         # Add DateFilter dependency
         if bot_name == 'fetch':
@@ -145,17 +178,17 @@ def main():
             date_filter.set_debugger(debugger)
             date_filter.set_logger(logger)
             bot.add_packet_filter(date_filter)
-            messages.append(f"packet_filter_class={date_filter.__class__.__name__}")
+            message_to_log.append(f"packet_filter_class={date_filter.__class__.__name__}")
         elif bot_name == 'init':
             name_filter = filt.NameFilter()
             name_filter.set_logger(logger)
             name_filter.set_debugger(debugger)
             bot.add_packet_filter(name_filter)
-            messages.append(f"packet_filter_class={name_filter.__class__.__name__}")
+            message_to_log.append(f"packet_filter_class={name_filter.__class__.__name__}")
 
         # Get InsertionQueryExecutor class
         insertion_executor_class = ins.get_insertion_executor_class(sensor_type)
-        insertion_executor = insertion_executor_class(query_builder=query_builder, conn=dbconn, timest_cls=timest_cls)
+        insertion_executor = insertion_executor_class(query_builder=query_builder, conn=db_adapter, timest_cls=timest_cls)
         insertion_executor.set_debugger(debugger)
         insertion_executor.set_logger(logger)
 
@@ -165,46 +198,46 @@ def main():
 
         # Add InsertionQueryExecutor dependency
         bot.add_insertion_executor(insertion_executor)
-        messages.append(f"insertion_executor_class={insertion_executor_class.__name__}")
+        message_to_log.append(f"insertion_executor_class={insertion_executor_class.__name__}")
 
         if bot_name in ('init', 'update'):
             sensor_rshp_class = sens.get_sensor_adapter_class(sensor_type)
             bot.add_sensor_rshp_class(sensor_rshp_class)
-            messages.append(f"sensor_reshaper_class={sensor_rshp_class.__name__}")
+            message_to_log.append(f"sensor_reshaper_class={sensor_rshp_class.__name__}")
 
         # ParamReshaper class
         if bot_name == 'fetch':
             param_rshp_class = par.get_param_adapter_class(sensor_type)
             bot.add_param_rshp_class(param_rshp_class)
-            messages.append(f"param_reshaper_class={param_rshp_class.__name__}")
+            message_to_log.append(f"param_reshaper_class={param_rshp_class.__name__}")
 
         # MeasureReshaper class
         if bot_name == 'fetch':
             measure_rshp_class = meas.get_measure_adapter_class(sensor_type)
             bot.add_measure_rshp_class(measure_rshp_class)
-            messages.append(f"measure_reshaper_class={measure_rshp_class.__name__}")
+            message_to_log.append(f"measure_reshaper_class={measure_rshp_class.__name__}")
 
         # Add DateLooper dependency
         if bot_name == 'fetch':
             date_looper_cls = loop.get_date_looper_class(sensor_type)
             bot.add_date_looper_class(date_looper_cls)
-            messages.append(f"date_looper_class={date_looper_cls.__name__}")
+            message_to_log.append(f"date_looper_class={date_looper_cls.__name__}")
 
         # Add SensorQueryExecutor dependency
         if bot_name == 'fetch':
-            sensor_query_executor = sel.SensorQueryExecutor(conn=dbconn, query_builder=query_builder)
+            sensor_query_executor = sel.SensorQueryExecutor(conn=db_adapter, query_builder=query_builder)
             bot.add_sensor_query_executor(sensor_query_executor)
-            messages.append(f"sensor_query_executor={sensor_query_executor.__class__.__name__}")
+            message_to_log.append(f"sensor_query_executor={sensor_query_executor.__class__.__name__}")
 
         # Add BotQueryExecutor dependency
-        bot_query_executor = sel.BotQueryExecutor(conn=dbconn, query_builder=query_builder, sensor_type=sensor_type)
+        bot_query_executor = sel.BotQueryExecutor(conn=db_adapter, query_builder=query_builder, sensor_type=sensor_type)
         bot_query_executor.set_logger(logger)
         bot_query_executor.set_debugger(debugger)
         bot.add_bot_query_executor(bot_query_executor)
-        messages.append(f"bot_query_executor={bot_query_executor.__class__.__name__}")
+        message_to_log.append(f"bot_query_executor={bot_query_executor.__class__.__name__}")
 
         # Debug and log the program settings
-        for msg in messages:
+        for msg in message_to_log:
             debugger.debug(msg)
             logger.debug(msg)
 
@@ -218,7 +251,7 @@ def main():
         bot.run()
 
         # Close database connection
-        dbconn.close_conn()
+        db_adapter.close_conn()
         debugger.info("success => database connection closed")
         logger.info("success => database connection closed")
 
