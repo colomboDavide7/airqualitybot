fcebb3c4839bd628bda555a977f487a41da611f4
Davide Colombo <davide.colombo02@universitadipavia.it>
2021-11-29 22:34:15 +0100

    SUCCESS => refactor commands + added tests

--- test/file/util/test_loader.py
+++ test/file/util/test_loader.py
@@ -8,6 +8,7 @@
 import os
 import unittest
 import airquality.file.util.loader as fl
+import airquality.file.structured.json as jf
 
 
 class TestFileLoader(unittest.TestCase):
@@ -17,14 +18,18 @@ class TestFileLoader(unittest.TestCase):
             os.environ.pop("DBCONN")
 
     def test_exit_on_missing_database_connection_string_property(self):
-        test_environment_path = "test/file/.empty_env"
+        test_environment_path = "test/file/util/.empty_env"
         with self.assertRaises(SystemExit):
             fl.load_environment_file(file_path=test_environment_path, sensor_type="atmotube")
 
     def test_exit_on_missing_secret_purpleair_api_key(self):
-        test_environment_path = "test/file/.missing_purpleair_env"
+        test_environment_path = "test/file/util/.missing_purpleair_env"
         with self.assertRaises(SystemExit):
             fl.load_environment_file(file_path=test_environment_path, sensor_type="purpleair")
+
+    def test_successfully_get_json_file(self):
+        actual = fl.load_structured_file(file_path="test/file/util/test_loader.json")
+        self.assertEqual(actual.__class__, jf.JSONFile)
 
 
 if __name__ == '__main__':
