85afa5f47622b995dc8d6b8ef11e8843fb97af8a
Davide Colombo <davide.colombo02@universitadipavia.it>
2021-12-30 19:05:41 +0100

    Inline variable refactoring in request builder

--- airquality/request_builder.py
+++ airquality/request_builder.py
@@ -22,18 +22,19 @@ class AddPurpleairSensorRequestBuilder(IterableItemsABC):
 
     def items(self) -> Generator[AddFixedSensorRequest, None, None]:
         for dm in self.datamodel:
-            last_acquisition = datetime.fromtimestamp(dm.date_created)
+            created_at = datetime.fromtimestamp(dm.date_created)
             channels = [
-                Channel(api_key=dm.primary_key_a, api_id=str(dm.primary_id_a), channel_name="1A", last_acquisition=last_acquisition),
-                Channel(api_key=dm.primary_key_b, api_id=str(dm.primary_id_b), channel_name="1B", last_acquisition=last_acquisition),
-                Channel(api_key=dm.secondary_key_a, api_id=str(dm.secondary_id_a), channel_name="2A", last_acquisition=last_acquisition),
-                Channel(api_key=dm.secondary_key_b, api_id=str(dm.secondary_id_b), channel_name="2B", last_acquisition=last_acquisition)
+                Channel(api_key=dm.primary_key_a, api_id=str(dm.primary_id_a), channel_name="1A", last_acquisition=created_at),
+                Channel(api_key=dm.primary_key_b, api_id=str(dm.primary_id_b), channel_name="1B", last_acquisition=created_at),
+                Channel(api_key=dm.secondary_key_a, api_id=str(dm.secondary_id_a), channel_name="2A", last_acquisition=created_at),
+                Channel(api_key=dm.secondary_key_b, api_id=str(dm.secondary_id_b), channel_name="2B", last_acquisition=created_at)
             ]
-
-            geolocation = Geolocation(latitude=dm.latitude, longitude=dm.longitude)
-            name = f"{dm.name} ({dm.sensor_index})"
-
-            yield AddFixedSensorRequest(type="Purpleair/Thingspeak", name=name, channels=channels, geolocation=geolocation)
+            yield AddFixedSensorRequest(
+                type="Purpleair/Thingspeak",
+                name=f"{dm.name} ({dm.sensor_index})",
+                channels=channels,
+                geolocation=Geolocation(latitude=dm.latitude, longitude=dm.longitude)
+            )
 
 
 class AddAtmotubeMeasureRequestBuilder(IterableItemsABC):
@@ -42,7 +43,7 @@ class AddAtmotubeMeasureRequestBuilder(IterableItemsABC):
     a set of *AtmotubeDatamodel* items into an *AddMobileMeasureRequest* Generator.
     """
 
-    ATMOTUBE_TIMESTAMP_FMT = "%Y-%m-%dT%H:%M:%S.000Z"
+    TIMESTAMP_FMT = "%Y-%m-%dT%H:%M:%S.000Z"
 
     def __init__(self, datamodel: IterableItemsABC, code2id: Dict[str, int]):
         self.datamodel = datamodel
@@ -50,10 +51,6 @@ class AddAtmotubeMeasureRequestBuilder(IterableItemsABC):
 
     def items(self) -> Generator[AddMobileMeasureRequest, None, None]:
         for dm in self.datamodel:
-            coords = dm.coords
-            geolocation = Geolocation(latitude=coords['lat'], longitude=coords['lon'])
-            timestamp = datetime.strptime(dm.time, self.ATMOTUBE_TIMESTAMP_FMT)
-
             measures = [
                 (self.code2id['voc'], dm.voc),
                 (self.code2id['pm1'], dm.pm1),
@@ -64,4 +61,8 @@ class AddAtmotubeMeasureRequestBuilder(IterableItemsABC):
                 (self.code2id['p'], dm.p)
             ]
 
-            yield AddMobileMeasureRequest(timestamp=timestamp, geolocation=geolocation, measures=measures)
+            yield AddMobileMeasureRequest(
+                timestamp=datetime.strptime(dm.time, self.TIMESTAMP_FMT),
+                geolocation=Geolocation(latitude=dm.coords['lat'], longitude=dm.coords['lon']),
+                measures=measures
+            )
