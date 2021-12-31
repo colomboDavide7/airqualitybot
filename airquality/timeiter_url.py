1eed2079c8c172c0ae762c78c314d0023ee882ca
Davide Colombo <davide.colombo02@universitadipavia.it>
2021-12-31 14:43:49 +0100

    Added atmotube time iterable url

---
+++ airquality/timeiter_url.py
@@ -0,0 +1,32 @@
+######################################################
+#
+# Author: Davide Colombo
+# Date: 31/12/21 14:33
+# Description: INSERT HERE THE DESCRIPTION
+#
+######################################################
+from airquality.iteritems import IterableItemsABC
+from datetime import datetime, timedelta
+from typing import Generator
+
+
+class AtmotubeTimeIterableURL(IterableItemsABC):
+    """
+    An *IterableItemsABC* that defines the business rules for properly format an Atmotube URL by adding
+    the *date* parameter in the querystring.
+    The output is a Generator of formatted urls starting from *begin* to *until*.
+    """
+
+    def __init__(self, url: str, begin: datetime, until=datetime.now(), step_size_in_days=1):
+        self.url = url + "&date={date}"
+        self.begin = begin
+        self.until = until
+        self.step_size_in_days = step_size_in_days
+
+    def items(self) -> Generator[str, None, None]:
+        tmp_begin = self.begin
+        tmp_until = self.until
+        while tmp_begin <= tmp_until:
+            tmp_date = tmp_until if tmp_begin >= tmp_until else tmp_begin
+            yield self.url.format(date=tmp_date.date().strftime("%Y-%m-%d"))
+            tmp_begin += timedelta(days=self.step_size_in_days)
---
+++ test/test_time_iterable_url.py
@@ -0,0 +1,34 @@
+######################################################
+#
+# Author: Davide Colombo
+# Date: 31/12/21 14:26
+# Description: INSERT HERE THE DESCRIPTION
+#
+######################################################
+from airquality.timeiter_url import AtmotubeTimeIterableURL
+from unittest import TestCase, main
+from datetime import datetime
+
+
+class TestTimeIterableURL(TestCase):
+
+    def test_atmotube_url_formatter(self):
+        test_begin = datetime.strptime('2021-12-01 00:00:00', "%Y-%m-%d %H:%M:%S")
+        test_until = datetime.strptime('2021-12-03 17:44:35', "%Y-%m-%d %H:%M:%S")
+
+        urls = AtmotubeTimeIterableURL(url="some_url", begin=test_begin, until=test_until, step_size_in_days=1)
+        self.assertEqual(len(urls), 3)
+
+        url1 = urls[0]
+        self.assertEqual(url1, "some_url&date=2021-12-01")
+        url2 = urls[1]
+        self.assertEqual(url2, "some_url&date=2021-12-02")
+        url3 = urls[2]
+        self.assertEqual(url3, "some_url&date=2021-12-03")
+
+        with self.assertRaises(IndexError):
+            print(urls[3])
+
+
+if __name__ == '__main__':
+    main()
