f65c7b27f2ad58dd9dbd42d341585660e3b904fd
Davide Colombo <davide.colombo02@universitadipavia.it>
2022-01-01 20:42:40 +0100

    Added abstract method for process usecases in usecase runner

--- airquality/usecase_runner.py
+++ airquality/usecase_runner.py
@@ -20,8 +20,16 @@ class UsecaseRunner(object):
         self.env = env
         self.personality = personality
 
-    @abstractmethod
     def run(self):
+        with Psycopg2Adapter(dbname=self.env.dbname,
+                             user=self.env.user,
+                             password=self.env.password,
+                             host=self.env.host,
+                             port=self.env.port) as dbadapter:
+            self.process_usecases(gateway=DatabaseGateway(dbadapter=dbadapter))
+
+    @abstractmethod
+    def process_usecases(self, gateway: DatabaseGateway) -> None:
         pass
 
 
@@ -34,22 +42,15 @@ class AddFixedSensorsRunner(UsecaseRunner):
     A *UsecaseRunner* that defines how to run an *AddFixedSensors* UseCase.
     """
 
-    def run(self):
-        with Psycopg2Adapter(dbname=self.env.dbname,
-                             user=self.env.user,
-                             password=self.env.password,
-                             host=self.env.host,
-                             port=self.env.port) as dbadapter:
-
-            gateway = DatabaseGateway(dbadapter=dbadapter)
-            start_sensor_id = gateway.get_max_sensor_id_plus_one()
-            existing_names = gateway.get_existing_sensor_names_of_type(sensor_type=self.personality)
-            datamodels = PurpleairAPIDataBuilder(url=self.env.url_template(personality=self.personality))
-            AddFixedSensors(
-                output_gateway=gateway,
-                existing_names=existing_names,
-                start_sensor_id=start_sensor_id
-            ).process(datamodels=datamodels)
+    def process_usecases(self, gateway: DatabaseGateway) -> None:
+        start_sensor_id = gateway.get_max_sensor_id_plus_one()
+        existing_names = gateway.get_existing_sensor_names_of_type(sensor_type=self.personality)
+        datamodels = PurpleairAPIDataBuilder(url=self.env.url_template(personality=self.personality))
+        AddFixedSensors(
+            output_gateway=gateway,
+            existing_names=existing_names,
+            start_sensor_id=start_sensor_id
+        ).process(datamodels=datamodels)
 
 
 from airquality.url.timeiter_url import AtmotubeTimeIterableURL
@@ -62,31 +63,26 @@ class AddMobileMeasuresRunner(UsecaseRunner):
     A *UsecaseRunner* that defines how to run the *AddMobileMeasures* UseCase.
     """
 
-    def run(self):
-        with Psycopg2Adapter(dbname=self.env.dbname,
-                             user=self.env.user,
-                             password=self.env.password,
-                             host=self.env.host,
-                             port=self.env.port) as dbadapter:
-
-            gateway = DatabaseGateway(dbadapter=dbadapter)
-            url_template = self.env.url_template(personality=self.personality)
-            code2id = gateway.get_measure_param_owned_by(owner=self.personality)
-            apiparam = gateway.get_apiparam_of_type(sensor_type=self.personality)
-            for param in apiparam:
-                pre_formatted_url = url_template.format(api_key=param.api_key, api_id=param.api_id, api_fmt="json")
-                urls = AtmotubeTimeIterableURL(url=pre_formatted_url, begin=param.last_acquisition)
-                for url in urls:
-                    start_packet_id = gateway.get_max_mobile_packet_id_plus_one()
-                    filter_ts = gateway.get_last_acquisition_of_sensor_channel(sensor_id=param.sensor_id, ch_name=param.ch_name)
-                    AddMobileMeasures(
-                        output_gateway=gateway,
-                        start_packet_id=start_packet_id,
-                        sensor_id=param.sensor_id,
-                        ch_name=param.ch_name,
-                        filter_ts=filter_ts,
-                        code2id=code2id
-                    ).process(datamodels=AtmotubeAPIDataBuilder(url=url))
+    def process_usecases(self, gateway: DatabaseGateway) -> None:
+        url_template = self.env.url_template(personality=self.personality)
+        code2id = gateway.get_measure_param_owned_by(owner=self.personality)
+        apiparam = gateway.get_apiparam_of_type(sensor_type=self.personality)
+        for param in apiparam:
+            print(repr(param))
+            pre_formatted_url = url_template.format(api_key=param.api_key, api_id=param.api_id, api_fmt="json")
+            urls = AtmotubeTimeIterableURL(url=pre_formatted_url, begin=param.last_acquisition)
+            for url in urls:
+                start_packet_id = gateway.get_max_mobile_packet_id_plus_one()
+                filter_ts = gateway.get_last_acquisition_of_sensor_channel(sensor_id=param.sensor_id, ch_name=param.ch_name)
+                print(f"url='{url}', packet_id='{start_packet_id}', filter_ts='{filter_ts}'")
+                AddMobileMeasures(
+                    output_gateway=gateway,
+                    start_packet_id=start_packet_id,
+                    sensor_id=param.sensor_id,
+                    ch_name=param.ch_name,
+                    filter_ts=filter_ts,
+                    code2id=code2id
+                ).process(datamodels=AtmotubeAPIDataBuilder(url=url))
 
 
 from airquality.url.timeiter_url import ThingspeakTimeIterableURL
@@ -108,30 +104,24 @@ class AddStationMeasuresRunner(UsecaseRunner):
                   'field4': '2.5_um_count_b', 'field5': '5.0_um_count_b', 'field6': '10.0_um_count_b'}
     FIELD_MAP = {'1A': MAPPING_1A, '1B': MAPPING_1B, '2A': MAPPING_2A, '2B': MAPPING_2B}
 
-    def run(self):
-        with Psycopg2Adapter(dbname=self.env.dbname,
-                             user=self.env.user,
-                             password=self.env.password,
-                             host=self.env.host,
-                             port=self.env.port) as dbadapter:
-            gateway = DatabaseGateway(dbadapter=dbadapter)
-            url_template = self.env.url_template(personality=self.personality)
-            code2id = gateway.get_measure_param_owned_by(owner=self.personality)
-            apiparam = gateway.get_apiparam_of_type(sensor_type=self.personality)
-            for param in apiparam:
-                print(repr(param))
-                pre_formatted_url = url_template.format(api_key=param.api_key, api_id=param.api_id, api_fmt="json")
-                urls = ThingspeakTimeIterableURL(url=pre_formatted_url, begin=param.last_acquisition, step_size_in_days=7)
-                for url in urls:
-                    start_packet_id = gateway.get_max_station_packet_id_plus_one()
-                    filter_ts = gateway.get_last_acquisition_of_sensor_channel(sensor_id=param.sensor_id, ch_name=param.ch_name)
-                    print(f"url='{url}', packet_id='{start_packet_id}', filter_ts='{filter_ts}'")
-                    AddStationMeasures(
-                        output_gateway=gateway,
-                        filter_ts=filter_ts,
-                        code2id=code2id,
-                        field_map=self.FIELD_MAP[param.ch_name],
-                        start_packet_id=start_packet_id,
-                        sensor_id=param.sensor_id,
-                        ch_name=param.ch_name
-                    ).process(datamodels=ThingspeakAPIDataBuilder(url=url))
+    def process_usecases(self, gateway: DatabaseGateway) -> None:
+        url_template = self.env.url_template(personality=self.personality)
+        code2id = gateway.get_measure_param_owned_by(owner=self.personality)
+        apiparam = gateway.get_apiparam_of_type(sensor_type=self.personality)
+        for param in apiparam:
+            print(repr(param))
+            pre_formatted_url = url_template.format(api_key=param.api_key, api_id=param.api_id, api_fmt="json")
+            urls = ThingspeakTimeIterableURL(url=pre_formatted_url, begin=param.last_acquisition, step_size_in_days=7)
+            for url in urls:
+                start_packet_id = gateway.get_max_station_packet_id_plus_one()
+                filter_ts = gateway.get_last_acquisition_of_sensor_channel(sensor_id=param.sensor_id, ch_name=param.ch_name)
+                print(f"url='{url}', packet_id='{start_packet_id}', filter_ts='{filter_ts}'")
+                AddStationMeasures(
+                    output_gateway=gateway,
+                    filter_ts=filter_ts,
+                    code2id=code2id,
+                    field_map=self.FIELD_MAP[param.ch_name],
+                    start_packet_id=start_packet_id,
+                    sensor_id=param.sensor_id,
+                    ch_name=param.ch_name
+                ).process(datamodels=ThingspeakAPIDataBuilder(url=url))
