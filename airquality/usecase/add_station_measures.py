071fb7434868cee046ede4fa1f50b0bba19fd879
Davide Colombo <davide.colombo02@universitadipavia.it>
2022-01-01 17:08:48 +0100

    Added add station measures usecase

--- airquality/usecase/add_station_measures.py
+++ airquality/usecase/add_station_measures.py
@@ -1,51 +1,56 @@
 ######################################################
 #
 # Author: Davide Colombo
-# Date: 31/12/21 11:54
+# Date: 01/01/22 16:53
 # Description: INSERT HERE THE DESCRIPTION
 #
 ######################################################
-from typing import Dict
-from datetime import datetime
-from airquality.database.gateway import DatabaseGateway
-from airquality.core.apidata_builder import AtmotubeAPIDataBuilder
-from airquality.core.request_builder import AddAtmotubeMeasureRequestBuilder
+from airquality.core.apidata_builder import ThingspeakAPIDataBuilder
+from airquality.core.request_builder import AddThingspeakMeasuresRequestBuilder
 from airquality.core.request_validator import AddSensorMeasuresRequestValidator
-from airquality.core.response_builder import AddMobileMeasureResponseBuilder
+from airquality.core.response_builder import AddStationMeasuresResponseBuilder
+from airquality.database.gateway import DatabaseGateway
+from datetime import datetime
+from typing import Dict
 
 
-class AddMobileMeasures(object):
+class AddStationMeasures(object):
     """
-    An *object* that represents the UseCase of adding mobile measurements to the database
+    An *object* that represents the UseCase of adding station measurements to the database
     through the *output_gateway*.
     """
 
     def __init__(
             self,
             output_gateway: DatabaseGateway,            # The database output boundary.
-            code2id: Dict[str, int],                    # The map between measure param code and id.
             filter_ts: datetime,                        # The timestamp used to validate the requests.
+            code2id: Dict[str, int],                    # The map between measure param code and id.
+            field_map: Dict[str, str],                  # The map between the API fields and database param code.
             start_packet_id: int,                       # The id from where to start inserting all the packets.
             sensor_id: int,                             # The unique id of the sensor that collects the measurements.
             ch_name: str                                # The sensor's acquisition channel name.
     ):
         self.output_gateway = output_gateway
-        self.code2id = code2id
         self.filter_ts = filter_ts
+        self.code2id = code2id
+        self.field_map = field_map
         self.start_packet_id = start_packet_id
         self.sensor_id = sensor_id
         self.ch_name = ch_name
 
-    def process(self, datamodels: AtmotubeAPIDataBuilder):
+    def process(self, datamodels: ThingspeakAPIDataBuilder):
         print(f"found #{len(datamodels)} datamodels")
-        requests = AddAtmotubeMeasureRequestBuilder(datamodel=datamodels, code2id=self.code2id)
+        requests = AddThingspeakMeasuresRequestBuilder(datamodel=datamodels, code2id=self.code2id, field_map=self.field_map)
         print(f"found #{len(requests)} requests")
         valid_requests = AddSensorMeasuresRequestValidator(request=requests, filter_ts=self.filter_ts)
         print(f"found #{len(valid_requests)} valid requests")
-        responses = AddMobileMeasureResponseBuilder(requests=valid_requests, start_packet_id=self.start_packet_id)
-
+        responses = AddStationMeasuresResponseBuilder(
+            requests=valid_requests, start_packet_id=self.start_packet_id, sensor_id=self.sensor_id
+        )
         if responses:
             print(f"found responses within [{valid_requests[0].timestamp!s} - {valid_requests[-1].timestamp!s}]")
-            self.output_gateway.insert_mobile_sensor_measures(responses=responses)
+            self.output_gateway.insert_station_measures(responses=responses)
             last_acquisition = valid_requests[-1].timestamp.strftime("%Y-%m-%d %H:%M:%S")
-            self.output_gateway.update_last_acquisition(timestamp=last_acquisition, sensor_id=self.sensor_id, ch_name=self.ch_name)
+            self.output_gateway.update_last_acquisition(
+                timestamp=last_acquisition, sensor_id=self.sensor_id, ch_name=self.ch_name
+            )
--- test/usecase/test_add_station_measures.py
+++ test/usecase/test_add_station_measures.py
@@ -5,17 +5,65 @@
 # Description: INSERT HERE THE DESCRIPTION
 #
 ######################################################
+from datetime import datetime
 from unittest import TestCase, main
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock
+from airquality.datamodel.apidata import ThingspeakAPIData
+from airquality.usecase.add_station_measures import AddStationMeasures
 
 
 class TestAddStationMeasuresUsecase(TestCase):
-    pass
 
-    # def test_add_station_measures(self):
-    #
-    #     mocked_gateway = MagicMock()
-    #     mocked_gateway
+    @property
+    def get_test_thingspeak_apidata_model(self):
+        return ThingspeakAPIData(
+            created_at="2021-12-20T11:18:40Z",
+            field1="20.50",
+            field2="35.53",
+            field3="37.43",
+            field6="55",
+            field7="60"
+        )
+
+    @property
+    def get_test_filter_timestamp(self):
+        return datetime.strptime("2021-12-09T15:52:34Z", "%Y-%m-%dT%H:%M:%SZ")
+
+    @property
+    def get_test_field_map(self):
+        return {'field1': 'p1', 'field2': 'p2', 'field3': 'p3', 'field6': 'p6', 'field7': 'p7'}
+
+    @property
+    def get_test_code2id(self):
+        return {'p1': 12, 'p2': 13, 'p3': 14, 'p6': 15, 'p7': 16}
+
+    def test_add_station_measures(self):
+        mocked_thingspeak_datamodel = MagicMock()
+        mocked_thingspeak_datamodel.__len__.return_value = 1
+        mocked_thingspeak_datamodel.__iter__.return_value = [self.get_test_thingspeak_apidata_model]
+
+        mocked_gateway = MagicMock()
+        mocked_gateway.insert_station_measures = MagicMock()
+        mocked_gateway.update_last_acquisition = MagicMock()
+
+        AddStationMeasures(
+            output_gateway=mocked_gateway,
+            filter_ts=self.get_test_filter_timestamp,
+            code2id=self.get_test_code2id,
+            field_map=self.get_test_field_map,
+            start_packet_id=140,
+            sensor_id=99,
+            ch_name="fakename"
+        ).process(datamodels=mocked_thingspeak_datamodel)
+
+        responses = mocked_gateway.insert_station_measures.call_args[1]['responses']
+        self.assertEqual(len(responses), 1)
+
+        resp = responses[0]
+        expected_measure_record = "(140, 99, 12, 20.5, '2021-12-20 11:18:40'),(140, 99, 13, 35.53, '2021-12-20 11:18:40')," \
+                                  "(140, 99, 14, 37.43, '2021-12-20 11:18:40'),(140, 99, 15, 55.0, '2021-12-20 11:18:40')," \
+                                  "(140, 99, 16, 60.0, '2021-12-20 11:18:40')"
+        self.assertEqual(resp.measure_record, expected_measure_record)
 
 
 if __name__ == '__main__':
