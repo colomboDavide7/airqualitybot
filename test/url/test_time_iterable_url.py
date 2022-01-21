d17f20b9a359c0a5491ac67a7f0a9cebe80012cc
Davide Colombo <davide.colombo02@universitadipavia.it>
2022-01-21 08:38:45 +0100

    Refactor test time iterable url

--- test/url/test_time_iterable_url.py
+++ test/url/test_time_iterable_url.py
@@ -10,39 +10,68 @@ from unittest import TestCase, main
 from datetime import datetime
 
 
-class TestTimeIterableURL(TestCase):
+# ======================================== THINGSPEAK TESTS ========================================
+def _expected_thingspeak_repr():
+    return "ThingspeakTimeIterableURL(" \
+           "url='some_url', " \
+           "begin='2021-12-01 00:00:00', " \
+           "until='2021-12-29 17:44:35', " \
+           "step_size_in_days='7')"
 
-    def test_atmotube_url_formatter(self):
-        test_begin = datetime.strptime('2021-12-01 00:00:00', "%Y-%m-%d %H:%M:%S")
-        test_until = datetime.strptime('2021-12-03 17:44:35', "%Y-%m-%d %H:%M:%S")
 
-        urls = AtmotubeTimeIterableURL(url="some_url", begin=test_begin, until=test_until, step_size_in_days=1)
-        self.assertEqual(len(urls), 3)
+class TestThingspeakTimeIterableURL(TestCase):
 
-        url1 = urls[0]
-        self.assertEqual(url1, "some_url&date=2021-12-01")
-        url2 = urls[1]
-        self.assertEqual(url2, "some_url&date=2021-12-02")
-        url3 = urls[2]
-        self.assertEqual(url3, "some_url&date=2021-12-03")
+# =========== SETUP METHOD
+    def setUp(self) -> None:
+        self._begin = datetime.strptime('2021-12-01 00:00:00', "%Y-%m-%d %H:%M:%S")
+        self._until = datetime.strptime('2021-12-29 17:44:35', "%Y-%m-%d %H:%M:%S")
+        self._urls = ThingspeakTimeIterableURL(url="some_url",
+                                               begin=self._begin,
+                                               until=self._until,
+                                               step_size_in_days=7)
 
-        with self.assertRaises(IndexError):
-            print(urls[3])
-
+# =========== TEST METHODS
     def test_thingspeak_url_formatter(self):
-        test_begin = datetime.strptime('2021-12-01 00:00:00', "%Y-%m-%d %H:%M:%S")
-        test_until = datetime.strptime('2021-12-29 17:44:35', "%Y-%m-%d %H:%M:%S")
+        self.assertEqual(len(self._urls), 5)
+        self.assertEqual(self._urls[0], "some_url&start=2021-12-01%2000:00:00&end=2021-12-08%2000:00:00")
+        self.assertEqual(self._urls[1], "some_url&start=2021-12-08%2000:00:00&end=2021-12-15%2000:00:00")
+        self.assertEqual(self._urls[2], "some_url&start=2021-12-15%2000:00:00&end=2021-12-22%2000:00:00")
+        self.assertEqual(self._urls[3], "some_url&start=2021-12-22%2000:00:00&end=2021-12-29%2000:00:00")
+        self.assertEqual(self._urls[4], "some_url&start=2021-12-29%2000:00:00&end=2021-12-29%2017:44:35")
+        self.assertEqual(repr(self._urls), _expected_thingspeak_repr())
+        with self.assertRaises(IndexError):
+            print(self._urls[5])
 
-        urls = ThingspeakTimeIterableURL(url="some_url", begin=test_begin, until=test_until, step_size_in_days=7)
-        self.assertEqual(len(urls), 5)
-        self.assertEqual(urls[0], "some_url&start=2021-12-01%2000:00:00&end=2021-12-08%2000:00:00")
-        self.assertEqual(urls[1], "some_url&start=2021-12-08%2000:00:00&end=2021-12-15%2000:00:00")
-        self.assertEqual(urls[2], "some_url&start=2021-12-15%2000:00:00&end=2021-12-22%2000:00:00")
-        self.assertEqual(urls[3], "some_url&start=2021-12-22%2000:00:00&end=2021-12-29%2000:00:00")
-        self.assertEqual(urls[4], "some_url&start=2021-12-29%2000:00:00&end=2021-12-29%2017:44:35")
-        expected_repr = "ThingspeakTimeIterableURL(url='some_url', begin='2021-12-01 00:00:00', " \
-                        "until='2021-12-29 17:44:35', step_size_in_days='7')"
-        self.assertEqual(repr(urls), expected_repr)
+
+# ======================================== ATMOTUBE TESTS ========================================
+def _expected_atmotube_repr():
+    return "AtmotubeTimeIterableURL(" \
+           "url='some_url', " \
+           "begin='2021-12-01 00:00:00', " \
+           "until='2021-12-03 17:44:35', " \
+           "step_size_in_days='1')"
+
+
+class TestAtmotubeTimeIterableURL(TestCase):
+
+# =========== SETUP METHOD
+    def setUp(self) -> None:
+        self._begin = datetime.strptime('2021-12-01 00:00:00', "%Y-%m-%d %H:%M:%S")
+        self._until = datetime.strptime('2021-12-03 17:44:35', "%Y-%m-%d %H:%M:%S")
+        self._urls = AtmotubeTimeIterableURL(url="some_url",
+                                             begin=self._begin,
+                                             until=self._until,
+                                             step_size_in_days=1)
+
+# =========== TEST METHODS
+    def test_urls(self):
+        self.assertEqual(len(self._urls), 3)
+        self.assertEqual(self._urls[0], "some_url&date=2021-12-01")
+        self.assertEqual(self._urls[1], "some_url&date=2021-12-02")
+        self.assertEqual(self._urls[2], "some_url&date=2021-12-03")
+        self.assertEqual(repr(self._urls), _expected_atmotube_repr())
+        with self.assertRaises(IndexError):
+            print(self._urls[3])
 
 
 if __name__ == '__main__':
